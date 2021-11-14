c74f93bd673e9ce601389d6d2de36a0bdff45fa5
jessicamcg <jessicamcguico@gmail.com>
2021-11-14 16:33:23 -0600

    required morgan, logger

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,5 +22,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Vist app: http://localhost:${PORT}!`);
 });
