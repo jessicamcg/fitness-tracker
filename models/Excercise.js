2837dd45c0a06d064754702474cbb48310fdbcdf
jessicamcg <jessicamcguico@gmail.com>
2021-11-14 15:42:02 -0600

    workout and excerise models

--- models/Excercise.js
+++ models/Excercise.js
@@ -3,6 +3,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const excerciseSchema = new Schema ({
+    type: {
+        type: String,
+        trim: true,
+        required: "Enter type of excerise"
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter name of excerise"
+    },
+    duration: {
+        type: Number,
+        required: "Enter a duration"
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    }
 
 });
 
--- models/Workout.js
+++ models/Workout.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema ({
-
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: {
+        type: Array
+    }
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
