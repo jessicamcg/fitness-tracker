91475c1873ad08e1ee76ef62bf5890ea4f023f02
jessicamcg <jessicamcguico@gmail.com>
2021-11-14 16:24:09 -0600

    package.json

--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 const Excercise = require("../models/Excercise.js");
 
-
+// router.get("/api/workouts", (req,res) => {
+//     Workout.findall({})
+// })
 module.exports = router;
